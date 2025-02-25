da9ccd49c1f482f120f487cddbb25a83c07e4d7d
Tayuba <ayubamonnietahiru@gmail.com>
2025-02-26 00:32:30 +0100

    Book Interface Implemented

--- include/Book_Interface.hpp
+++ include/Book_Interface.hpp
@@ -1,5 +1,31 @@
 #ifndef BOOK_INTERFACE_H
 #define BOOK_INTERFACE_H
 
+#include "Constant_Headers.hpp"
+
+class Book_Interface{
+    private:
+    std::string title;
+    std::string author;
+    int year;
+    bool isBorrowed;
+
+public:
+    // Constructor
+    Book_Interface(const std::string& title, const std::string& author, int year);
+
+    // Getters
+    std::string getTitle() const;
+    std::string getAuthor() const;
+
+    bool getBorrowedStatus() const;
+
+    int getYear() const;
+
+    void borrowBook();
+    void returnBook();
+
+    std::string toString() const;
+};
 
 #endif
--- src/Book_Interface.cpp
+++ src/Book_Interface.cpp
@@ -1,1 +1,33 @@
 #include "../include/Book_Interface.hpp"
+
+
+Book_Interface::Book_Interface(const std::string& title, const std::string& author, int year)
+    : title(title), author(author), year(year), isBorrowed(false) {}
+
+std::string Book_Interface::getTitle() const {
+    return title;
+}
+
+std::string Book_Interface::getAuthor() const {
+    return author;
+}
+
+bool Book_Interface::getBorrowedStatus() const {
+    return isBorrowed;
+}
+
+void Book_Interface::borrowBook() {
+    isBorrowed = true;
+}
+
+void Book_Interface::returnBook() {
+    isBorrowed = false;
+}
+
+int Book_Interface::getYear() const {
+    return year;
+}
+
+std::string Book_Interface::toString() const {
+    return title + "," + author + "," + std::to_string(year) + "," + (isBorrowed ? "1" : "0");
+}
