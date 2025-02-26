34ffe33b4626abe221294e01801db6bc83a3adba
Tayuba <ayubamonnietahiru@gmail.com>
2025-02-27 00:23:20 +0100

    Book Interface Class fully commented pending the use of Doxygen to document code

--- include/Book_Interface.hpp
+++ include/Book_Interface.hpp
@@ -1,3 +1,17 @@
+/**
+ * @file Book_Interface.hpp
+ * @brief Interface for representing a book in the library system.
+ * 
+ * This file defines the Book_Interface class, which represents an individual book.
+ * It provides methods to retrieve book details and manage borrowing status.
+ * 
+ * Data Structure Used: Plain class with member variables.
+ * - Efficient O(1) access to book details.
+ * 
+ * @author Ayuba Tahiru
+ * @date 2025-02-25
+*/
+
 #ifndef BOOK_INTERFACE_H
 #define BOOK_INTERFACE_H
 
@@ -11,20 +25,54 @@ class Book_Interface{
     bool isBorrowed;
 
 public:
-    // Constructor
+    /**
+     * @class Book_Interface
+     * @brief Represents a book in the library system.
+     * 
+     * This class provides methods to retrieve book details, check borrowing status, 
+     * and manage borrowing and returning operations.
+    */
     Book_Interface(const std::string& title, const std::string& author, int year);
 
-    // Getters
+    /**
+     * @brief Retrieves the title of the book.
+     * @return std::string The book's title.
+    */
     std::string getTitle() const;
+
+    /**
+     * @brief Retrieves the author's name.
+     * @return std::string The book's author.
+    */
     std::string getAuthor() const;
 
+    /**
+     * @brief Checks if the book is currently borrowed.
+     * @return true If the book is borrowed.
+     * @return false If the book is available.
+    */
     bool getBorrowedStatus() const;
 
+    /**
+     * @brief Retrieves the publication year of the book.
+     * @return int The book's publication year.
+    */
     int getYear() const;
 
+    /**
+     * @brief Marks the book as borrowed.
+    */
     void borrowBook();
+
+    /**
+     * @brief Marks the book as returned.
+    */
     void returnBook();
 
+    /**
+     * @brief Converts book details into a string format.
+     * @return std::string A formatted string representation of the book.
+    */
     std::string toString() const;
 };
 
--- src/Book_Interface.cpp
+++ src/Book_Interface.cpp
@@ -1,33 +1,90 @@
+/**
+ * @file Book_Interface.cpp
+ * @brief Implementation of the Book_Interface class.
+ * 
+ * This file implements the Book_Interface class, defining methods for retrieving 
+ * book details and managing borrowing status.
+ * 
+ * Data Structure Used: Plain class with member variables.
+ * - O(1) access time for all operations since data is stored in class members.
+ * 
+*/
+
+
 #include "../include/Book_Interface.hpp"
 
-
+/**
+ * @brief Constructs a new Book_Interface object.
+ * 
+ * @param title The title of the book.
+ * @param author The author's name.
+ * @param year The publication year.
+*/
 Book_Interface::Book_Interface(const std::string& title, const std::string& author, int year)
     : title(title), author(author), year(year), isBorrowed(false) {}
 
+
+/**
+ * @brief Retrieves the title of the book.
+ * @return std::string The book's title.
+*/    
 std::string Book_Interface::getTitle() const {
     return title;
 }
 
+
+/**
+ * @brief Retrieves the author's name.
+ * @return std::string The book's author.
+*/
 std::string Book_Interface::getAuthor() const {
     return author;
 }
 
+
+/**
+ * @brief Checks if the book is currently borrowed.
+ * @return true If the book is borrowed.
+ * @return false If the book is available.
+*/
 bool Book_Interface::getBorrowedStatus() const {
     return isBorrowed;
 }
 
+
+/**
+ * @brief Marks the book as borrowed.
+ * 
+ * If the book is already borrowed, a warning message is displayed.
+*/
 void Book_Interface::borrowBook() {
     isBorrowed = true;
 }
 
+
+/**
+ * @brief Marks the book as returned.
+ * 
+ * If the book was not borrowed, a warning message is displayed.
+*/
 void Book_Interface::returnBook() {
     isBorrowed = false;
 }
 
+
+/**
+ * @brief Retrieves the publication year of the book.
+ * @return int The book's publication year.
+*/
 int Book_Interface::getYear() const {
     return year;
 }
 
+
+/**
+ * @brief Converts book details into a formatted string representation.
+ * @return std::string A formatted string containing book details.
+ */ 
 std::string Book_Interface::toString() const {
     return title + "," + author + "," + std::to_string(year) + "," + (isBorrowed ? "1" : "0");
 }
